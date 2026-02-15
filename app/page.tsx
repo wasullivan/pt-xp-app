5b2b6f51399659f2b3b3b8b2ff6d742231f35085
wasullivan <sullivan.william6@gmail.com>
2026-02-14 21:42:48 -0500

    Force error test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-// app/page.tsx
-import React from "react";
-
 export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>PT XP Tracker</h1>
-      <p>Your app is live on Vercel!</p>
-    </main>
-  );
+  throw new Error("If you see this error, deployment is correct.");
 }
